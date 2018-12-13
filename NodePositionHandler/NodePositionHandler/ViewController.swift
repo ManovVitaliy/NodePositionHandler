d12fce84f1f07f1288d7ef98760f2dd0a5999eaf
Vitaliy Manov <manovvitaliy@gmail.com>
2018-12-13 03:40:40 +0200

    task was done

--- NodePositionHandler/NodePositionHandler/ViewController.swift
+++ NodePositionHandler/NodePositionHandler/ViewController.swift
@@ -14,62 +14,75 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    var nodeWasAdded: Bool = false
+    var planeNode: SCNNode?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Set the view's delegate
         sceneView.delegate = self
-        
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
+        let scene = SCNScene()
         sceneView.scene = scene
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
-        // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
-        // Run the view's session
+        configuration.planeDetection = .horizontal
+        
         sceneView.session.run(configuration)
+        
+        sceneView.delegate = self
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
-        // Pause the view's session
         sceneView.session.pause()
     }
-
-    // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else {
+            return
+        }
         
+        guard nodeWasAdded == false else {
+            return
+        }
+        
+        let width = CGFloat(planeAnchor.extent.z) / 5
+        let height = CGFloat(planeAnchor.extent.z) / 5
+        let plane = SCNPlane(width: width, height: height)
+        
+        plane.materials.first?.diffuse.contents = UIColor.lightGray
+        
+        planeNode = SCNNode(geometry: plane)
+        
+        let x = CGFloat(planeAnchor.center.x)
+        let y = CGFloat(planeAnchor.center.y)
+        let z = CGFloat(planeAnchor.center.z)
+        planeNode?.position = SCNVector3(x, y, z)
+        planeNode?.eulerAngles.x = -.pi / 2
+        
+        node.addChildNode(planeNode!)
+        nodeWasAdded = true
     }
     
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
+    @IBAction func addToFrontButtonTapped(_ sender: Any) {
+        planeNode = sceneView.pointOfView
+        if let planeNode = planeNode {
+            planeNode.removeFromParentNode()
+            planeNode.position = SCNVector3Make(0, 0, -0.5)
+            planeNode.eulerAngles.x = 0
+            sceneView.pointOfView?.addChildNode(planeNode)
+        }
     }
     
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
+    @IBAction func removeFromFrontButtonTapped(_ sender: Any) {
+        if let planeNode = planeNode {
+            planeNode.removeFromParentNode()
+            self.planeNode = nil
+            nodeWasAdded = false
+        }
     }
 }
